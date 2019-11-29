8fdee97bae59933cd7c8769b65ca3b338faa30b1
Brian Løkke Borg <brianloekkeborg@gmail.com>
2019-11-29 11:26:44 +0100

    module completed

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,11 +9,11 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int OrbitedObjectId { get; set; }
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         
-        public int? OrbitedObject { get; set; }
+        public int? OrbitedObjectId { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
